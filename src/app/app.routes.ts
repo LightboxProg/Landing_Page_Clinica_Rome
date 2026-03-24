fed90bb56c7b8fe477ee50c57dbff197f2670169
WallaceGaster <saitama.kurosakyy@gmail.com>
2026-03-24 16:24:44 -0600

    creando esqueleto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { AboutComponent } from './paginas/about/about.component';
+import { ServicesComponent } from './paginas/services/services.component';
+import { ContactComponent } from './paginas/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' } // redirige a home si la ruta no existe
+];
