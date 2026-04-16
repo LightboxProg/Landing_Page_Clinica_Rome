14e1e2060e32ac8a190f4194302e37e18d85b3ee
Yahir <pouputopou4@gmail.com>
2026-04-16 14:19:22 -0600

    Refactor: Migración a estructura nativa de Angular, eliminación de iframe e integración global de assets y Slider Revolution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './componentes/header/header.component'; // Importa aquí
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent], // Añádelo aquí
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
