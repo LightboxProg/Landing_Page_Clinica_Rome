4beaab6e57ce075f5d7b87eff44243d3b51afbb7
oguzmarc14 <oguzmarc14@gmail.com>
2026-04-16 11:16:51 -0600

    fix(rutas): cambio en las rutas de todos los archivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './paginas/home/home.component';
-import { AboutComponent } from './paginas/about/about.component';
+
 import { ServicesComponent } from './paginas/services/services.component';
-import { ContactComponent } from './paginas/contact/contact.component';
 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent },
-  { path: 'services', component: ServicesComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '' } // redirige a home si la ruta no existe
+  { path: '', component: ServicesComponent },
+  { path: '**', redirectTo: '' } // redirige a services si la ruta no existe
 ];
